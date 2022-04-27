df78d5d07b56518915b7375da7e8c504eacc33ab
Mohammad Shahed <mohammadshahed5358@gmail.com>
2022-04-27 16:24:55 +0600

    login form and register form

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css';
 
 const Register = () => {
+
+    const navigate = useNavigate();
+
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+
+    const handleRegister = event => {
+        event.preventDefault();
+    }
     return (
-        <div>
-            <h2>Register</h2>
+        <div className="register-form">
+            <h2 style={{textAlign:'center'}}>Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" name="name" placeholder="Your Name" required />
+                <input type="email" name="email" placeholder="Email Address" required />
+                <input type="password" name="password" placeholder="Your Password" required/>
+                <input type="submit" value="Register" /> 
+            </form>
+            <p>Already have an account ? <Link to="/login" className="text-danger pe-auto text-decoration-none" onClick={navigateLogin}>Please Login</Link></p>
         </div>
     );
 };
